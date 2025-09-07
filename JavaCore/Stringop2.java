e2f2a66fd5d83eeca53290dc336f160feda0e5a6
Vedant Shirpurkar <shvedant007@gmail.com>
2025-09-07 11:59:13 +0530

    String Op2

--- JavaCore/Stringop2.java
+++ JavaCore/Stringop2.java
@@ -3,5 +3,9 @@ public class Stringop2 {
         String str1="Hello Java";
         int res1=str1.length();
         System.out.println("Length of String: "+res1);
+
+        //Contains
+        boolean res2=str1.contains("Java");
+        System.out.println("String Contains Java: "+res2);
     }
 }
