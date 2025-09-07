0d941d6a2ec4778ad50cac53db7388cf1f49973d
Vedant Shirpurkar <shvedant007@gmail.com>
2025-09-07 12:08:27 +0530

    String Op2 updated

--- JavaCore/Stringop2.java
+++ JavaCore/Stringop2.java
@@ -7,5 +7,11 @@ public class Stringop2 {
         //Contains
         boolean res2=str1.contains("Java");
         System.out.println("String Contains Java: "+res2);
+        
+        //Startswith and Endswith
+        boolean res3=str1.startsWith("Hello");
+        System.out.println("String Startswith : "+res3);
+        boolean res4=str1.endsWith("Hello");
+        System.out.println("String Startswith : "+res4);
     }
 }
