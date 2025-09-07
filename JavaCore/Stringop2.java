a0be0455878416494917d9d96307b081ee716349
Vedant Shirpurkar <shvedant007@gmail.com>
2025-09-07 11:53:06 +0530

    String Op2

---
+++ JavaCore/Stringop2.java
@@ -0,0 +1,7 @@
+public class Stringop2 {
+    public static void main(String args[]){
+        String str1="Hello Java";
+        int res1=str1.length();
+        System.out.println("Length of String: "+res1);
+    }
+}
