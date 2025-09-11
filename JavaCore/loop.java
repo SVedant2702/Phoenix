9c85988f59d17134787a8755663cb3cd80821627
Vedant Shirpurkar <shvedant007@gmail.com>
2025-09-11 17:14:40 +0530

    loop done

---
+++ JavaCore/loop.java
@@ -0,0 +1,12 @@
+public class loop{
+    public static void main(String [] args){
+        for(int i = 0; i<=10; i++){
+            System.out.println("hii");
+        }
+        int a = 0;
+        while(a<=3){
+            System.out.println("hellp");
+            a=a+1;
+        }
+    }
+}
